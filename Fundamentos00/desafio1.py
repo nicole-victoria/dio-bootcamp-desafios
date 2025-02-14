5f969b12b104349fa5726dfbd15ea940860a2538
nicole-victoria <nicolevlcosta.23@gmail.com>
2025-02-13 22:08:37 -0300

    primeiro desafio

---
+++ Fundamentos00/desafio1.py
@@ -0,0 +1,65 @@
+# Desafio: Criar um sistema bancário com as operações sacar, depositar e visualizar extrato
+menu = """
+=====MENU=====
+[1] Depositar
+[2] Sacar
+[3] Extrato
+[4] Sair
+
+=> """
+
+saldo = 0
+limite = 500
+extrato = ""
+numero_saques = 0
+LIMITE_SAQUES = 3
+
+while True:
+
+    opcao = input(menu)
+
+    if opcao == "1":
+        valor = float(input("Informe o valor do depósito: "))
+
+        if valor > 0:
+            saldo += valor
+            extrato += f"Depósito: R$ {valor:.2f}\n"
+
+        else:
+            print("Opa! Não é possível depositar esse valor")
+
+    elif opcao == "2":
+        valor = float(input("Informe o valor do saque: "))
+
+        if valor > saldo:
+            print("Operação falhou! Você não tem saldo suficiente.")
+
+        elif valor > limite:
+            print("Operação falhou! O valor do saque excede o limite.")
+
+        elif numero_saques >= LIMITE_SAQUES:
+            print("Operação falhou! Número máximo de saques excedido.")
+
+        elif valor > 0:
+            saldo -= valor
+            extrato += f"Saque: R$ {valor:.2f}\n"
+            numero_saques += 1
+
+        else:
+            print("Operação falhou! O valor informado é inválido.")
+
+    elif opcao == "3":
+        print("\n================ EXTRATO ================")
+        if extrato == "":
+            print("Não foram realizadas movimentações")
+        else:
+            print(extrato)
+        #print("Não foram realizadas movimentações." if not extrato else extrato)
+        print(f"\nSaldo: R$ {saldo:.2f}")
+        print("==========================================")
+
+    elif opcao == "4":
+        break
+
+    else:
+        print("Operação inválida, por favor selecione novamente a operação desejada.")
